08d0f5afd34835c53b5e5e41f2b7c29c172928df
hax <hans.larsen1994@gmail.com>
2019-11-13 14:31:49 +0100

    Plotter for axis.

---
+++ src/crustcrawler_controller/scripts/plotter.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+
+import rospy
+import numpy as np
+import matplotlib.pyplot as plt
+from std_msgs.msg import Float64MultiArray
+
+pos = [[],[],[],[]]
+actualPos = [[],[],[],[]]
+running = False
+firstRun = False
+
+def callbackActual(data):
+    global running
+    global firstRun
+
+    if not running:
+        return
+
+    actualPos[0].append(rospy.get_rostime().to_sec())
+    actualPos[1].append( data.data[0] )
+    actualPos[2].append( data.data[3] )
+    actualPos[3].append( data.data[6] )
+
+def callbackTraj(data):
+    global running
+    global firstRun
+
+    if not firstRun:
+        firstRun = True
+        running = True
+
+    if not running:
+        return
+
+    pos[0].append( rospy.get_rostime().to_sec() )
+    pos[1].append( data.data[0] )
+    pos[2].append( data.data[3] )
+    pos[3].append( data.data[6] )
+
+def listener():
+    global running
+    global firstRun
+
+    rospy.init_node('listener', anonymous=True)
+    rospy.Subscriber('/crustcrawler/getAngleVel', Float64MultiArray, callbackActual)
+    rospy.Subscriber('/crustcrawler/trajectory', Float64MultiArray, callbackTraj)
+
+    rate = rospy.Rate(10.0)
+    d = rospy.Duration.from_sec(20)
+
+    while not (rospy.is_shutdown()):
+        if not running:
+            rate.sleep()
+        else:
+            rospy.loginfo("Recording started")
+            rospy.sleep(d)
+            if (firstRun):
+                running = False
+
+            rospy.loginfo("Recording stopped")
+
+            fig, (ax1, ax2, ax3) = plt.subplots(3, sharey='col')
+
+            ax1.plot(actualPos[0], actualPos[1], color='red', label='Theta')
+            ax1.plot(pos[0], pos[1], color='blue', label='setTheta')
+            ax1.set_title("Joint 1")
+
+            ax2.plot(actualPos[0], actualPos[2], color='red', label='Theta')
+            ax2.plot(pos[0], pos[2], color='blue', label='setTheta')
+            ax2.set_title("Joint 2")
+
+            ax3.plot(actualPos[0], actualPos[3], color='red', label='Theta')
+            ax3.plot(pos[0], pos[3], color='blue', label='setTheta')
+            ax3.set_title("Joint 3")
+            plt.show()
+            return
+
+if __name__ == '__main__':
+    listener()
