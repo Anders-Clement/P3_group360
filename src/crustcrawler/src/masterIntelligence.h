1d09d4fafe8b7e9da1eac0b2979e172ad26001b0
Anders-Clement <andersclement96@gmial.com>
2019-11-25 12:03:04 +0100

    seperated to class

--- src/crustcrawler/src/masterIntelligence.h
+++ src/crustcrawler/src/masterIntelligence.h
@@ -1,10 +1,8 @@
-#include <string>
 #include <ros/ros.h>
-#include <sensor_msgs/JointState.h>
 #include <std_msgs/String.h>
-//#include <geometry_msgs/PoseStamped.h>
+#include <sensor_msgs/JointState.h>
+#include <sensor_msgs/Joy.h>
 #include <tf/transform_broadcaster.h>
-#include "std_msgs/Float64MultiArray.h"
 #include "std_msgs/Int16MultiArray.h"
 #include "std_msgs/Int16.h"
 #include <std_msgs/UInt8.h>
@@ -14,15 +12,12 @@
 #define move_pose 0.05
 #define tf 2.0
 
-#ifndef MASTER
-#define MASTER
-
-using namespace std;
-
+#ifndef CRUSTCRAWLER_MASTER_H
+#define CRUSTCRAWLER_MASTER_H
 
 class masterIntelligence {
 public:
-    masterIntelligence();
+    masterIntelligence(ros::NodeHandle* n, int type);
     // intialising the virables that will be used
     int gesture = 0;
     ros::Time gen_time;
@@ -30,7 +25,6 @@ public:
     float a[4][5] = {0};
     float eulerAng[3];
     float old_eulerAng[3];
-    bool modeChanged = true;
     bool update_angle_vel = true;
     float macro[4][4] = {0};
     float goalang[5];
@@ -41,32 +35,33 @@ public:
     float vel[5] = {0};
     float ang[5] = {0};
 
-
     // constucting functions
     void myo_raw_gest_str_callback(const std_msgs::String::ConstPtr& msg);
     void get_angle_vel_callback(const std_msgs::Int16MultiArray::ConstPtr& msg);
-    //void myo_raw_pose_callback(const geometry_msgs::PoseStamped::ConstPtr& msg);
+    void joy_callback(const sensor_msgs::Joy::ConstPtr& msg);
 
     void handleGesture();
 
+    //no longer used:
+    //void myo_raw_pose_callback(const geometry_msgs::PoseStamped::ConstPtr& msg);
+
+
 private:
 
-    ros::NodeHandle n;
+    ros::NodeHandle* n;
     ros::Publisher trajectory_pub;
     ros::Publisher joint_pub;
     ros::Publisher vibrate_pub;
     ros::Publisher mode_pub;
     ros::Subscriber gest_str_sub;
     ros::Subscriber get_angle_vel;
-    //ros::Subscriber pose_sub;
+    ros::Subscriber joy_sub;
 
     // message declarations
     std_msgs::Int16MultiArray trajectories;
     sensor_msgs::JointState joint_state;
     std_msgs::UInt8 vibrate;
     std_msgs::Int16 current_mode;
-
-
 };
 
 
